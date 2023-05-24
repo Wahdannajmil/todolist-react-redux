7e4089ca680f5b0ae32edba9f5fa576dc3da0281
Wahdan Najmil <najmilwahdan@gmail.com>
2023-05-24 08:54:23 +0700

    set background

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -7,6 +7,8 @@ import {
 } from 'react-icons/bs'
 import { useDispatch } from 'react-redux'
 import { actions } from '../features/todos/todosSlice'
+import '../assets/style/style.css'
+
 
 const TodoCard = ({ todo }) => {
   const dispatch = useDispatch()
@@ -45,7 +47,7 @@ const TodoCard = ({ todo }) => {
   }
 
   return (
-    <div className='flex justify-between p-4 bg-white rounded-2xl mb-4 items-center transition-all duration-300 hover:bg-gray-200'>
+    <div className='flex justify-between p-4 bg-white rounded-full mb-4 items-center transition-all duration-300 hover:bg-gray-200'>
       {editMode ? (
         <input
           type='text'
@@ -75,13 +77,17 @@ const TodoCard = ({ todo }) => {
         {editMode ? (
           <>
             <button
-              className='px-2 py-2 text-[rgb(73,72,69)] rounded transition-all duration-600 hover:text-gray-800'
+className =
+  'px-2 py-2 text-[rgb(73,72,69)] rounded transition-all duration-600 hover:text-black'
+
               onClick={handleUpdate}
             >
               <BsPencilSquare className='text-2xl' />
             </button>
             <button
-              className='px-2 py-2 text-[rgb(73,72,69)] rounded transition-all duration-600 hover:text-gray-800'
+className =
+  'px-2 py-2 text-[rgb(73,72,69)] rounded transition-all duration-600 hover:text-black'
+
               onClick={handleCancel}
             >
               <BsFillXCircleFill className='text-2xl' />
